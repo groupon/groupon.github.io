343f7ecddd3c64682c3a8debd0397f1cc1ed2784
Jesse Pinho <jesse@jessepinho.com>
2015-10-28 18:39:13 -0500

    Use Mustache templating to display projects from JSON

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+  var template = $("#template-project-categories").html(),
+      json = {
+        categories: [
+          {
+            title: "Continuous Integration",
+            projects: [
+              {
+                githubURL: "githubURL",
+                title: "Project name",
+                summary: "Project summary is a little longer than the title",
+                stars: 123
+              },
+              {
+                githubURL: "githubURL",
+                title: "Project name",
+                summary: "Project summary is a little longer than the title",
+                stars: 123
+              },
+              {
+                githubURL: "githubURL",
+                title: "Project name",
+                summary: "Project summary is a little longer than the title",
+                stars: 123
+              },
+              {
+                githubURL: "githubURL",
+                title: "Project name",
+                summary: "Project summary is a little longer than the title",
+                stars: 123
+              },
+            ]
+          }
+        ]
+      };
+  $("#projects").html(Mustache.render(template, json));
+});
