422f573160fa77e3a55747e4b528622f05cbf2cd
Aaron Bedra <aaron@aaronbedra.com>
2015-10-28 20:33:21 -0500

    All projects now added

--- js/main.js
+++ js/main.js
@@ -50,12 +50,135 @@ $(function() {
               {
                 githubURL: "https://github.com/groupon/assertive-as-promised",
                 title: "Assertive as Promised",
-                summary: "Extends assertive with promise support ",
+                summary: "Extends assertive with promise support",
                 stars: 1
               },
-
+              {
+                githubURL: "https://github.com/groupon/odo",
+                title: "Odo",
+                summary: "A Mock Proxy Server",
+                stars: 105
+              },
+              {
+                githubURL: "https://github.com/groupon/robo-remote",
+                title: "Robo Remote",
+                summary: "A remote control framework for Robotium",
+                stars: 32
+              },
+              {
+                githubURL: "https://github.com/groupon/retromock",
+                title: "Retromock",
+                summary: "Like Wiremock for Retrofit, but faster",
+                stars: 13
+              },
+              {
+                githubURL: "https://github.com/groupon/mysql-junit4",
+                title: "MySQL JUnit4",
+                summary: "JUnit 4 support for Tests against MySQL",
+                stars: 4
+              },
             ]
-          }
+          },
+          {
+            title: "Utility",
+            projects: [
+              {
+                githubURL: "https://github.com/groupon/webdriver-http-sync",
+                title: "Webdriver HTTP Sync",
+                summary: "Sync HTTP implementation of the WebDriver protocol for Node.js",
+                stars: 25
+              },
+              {
+                githubURL: "https://github.com/groupon/params_deserializers",
+                title: "Params Deserializers",
+                summary: "Deserializers for Rails params",
+                stars: 1
+              },
+              {
+                githubURL: "https://github.com/groupon/cson-parser",
+                title: "CSON Parser",
+                summary: "Simple & safe CSON parser",
+                stars: 84
+              },
+              {
+                githubURL: "https://github.com/groupon/jesos",
+                title: "Jesos",
+                summary: "A pure Java implementation of the Apache Mesos APIs",
+                stars: 46
+              },
+              {
+                githubURL: "https://github.com/groupon/gofer",
+                title: "Gofer",
+                summary: "A general purpose service client library for node.js",
+                stars: 36
+              },
+              {
+                githubURL: "https://github.com/groupon/mysql_slowlogd",
+                title: "MySQL Slowlogd",
+                summary: "Streaming MySQL slow query log over HTTP",
+                stars: 23
+              },
+              {
+                githubURL: "https://github.com/groupon/tdsql",
+                title: "tdsql",
+                summary: "Run SQL queries against a Teradata data warehouse server",
+                stars: 20
+              },
+              {
+                githubURL: "https://github.com/groupon/node-cached",
+                title: "Node Cached",
+                summary: "A simple caching library for node.js, inspired by the Play cache API",
+                stars: 21
+              },
+              {
+                githubURL: "https://github.com/groupon/sycl",
+                title: "sycl",
+                summary: "Simple YAML Config Library",
+                stars: 10
+              },
+              {
+                githubURL: "https://github.com/groupon/shellot",
+                title: "Shellot",
+                summary: "Slim terminal realtime graphing tool",
+                stars: 8
+              },
+              {
+                githubURL: "https://github.com/groupon/poller",
+                title: "Poller",
+                summary: "Poll a URL, and trigger code on changes",
+                stars: 8
+              },
+            ]
+          },
+	  {
+	    title: "Miscelaneous",
+	    projects: [
+              {
+                githubURL: "https://github.com/groupon/greenscreen",
+                title: "Greenscreen",
+                summary: "A digital signage solution using the Web and Chromecast devices",
+                stars: 863
+              },
+	      {
+                githubURL: "https://github.com/groupon/pebble",
+                title: "Pebble",
+                summary: "Efficient Web Graphs",
+                stars: 7
+              },
+              {
+                githubURL: "https://github.com/groupon/gleemail",
+                title: "Gleemail",
+                summary: "Making email template development fun! Sort of!",
+                stars: 280
+              },
+              {
+                githubURL: "https://github.com/groupon/roll",
+                title: "roll",
+                summary: "roll - bootstrap or upgrade a Unix host with Roller",
+                stars: 9
+              },
+	    ]
+	  }
         ]
       };
   $("#categories").html(Mustache.render(categoriesTemplate, json));
