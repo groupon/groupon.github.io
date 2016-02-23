6edd69a262416590ff9eff329f463839e8b7ed4b
Jesse Pinho <jesse@jessepinho.com>
2016-02-23 10:05:56 -0600

    Install Ember

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var categoriesTemplate = $("#template-categories").html(),
-      categoryListTemplate = $("#template-category-list").html();
-
-  $.getJSON("/json/repos.json", function(repos) {
-    $("#categories").html(Mustache.render(categoriesTemplate, repos));
-    $("#left-nav-categories").html(Mustache.render(categoryListTemplate, repos));
-  });
-});
