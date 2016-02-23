0e08d73bf57ffc59d837d95f1d1e4dee3581c87f
Jesse Pinho <jesse@jessepinho.com>
2016-02-23 14:58:16 -0600

    Prevent multiple fetches of repos
    This is done by only fetching them from the store once in the control, and not rendering parts of the template that use the repos until the repos have loaded (so as to prevent requests for individual repos).

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this._preloadRepos().then(() => {
-      return this.store.findAll('category');
-    });
-  },
-
-  _preloadRepos() {
-    return this.store.findAll('repo');
+    return this.store.findAll('category');
   }
 });
