ecab585729bc6c89186ee46a9cd9ce62a93b56b4
Jesse Pinho <jesse@jessepinho.com>
2016-02-23 15:48:17 -0600

    Use Octicons; show fork count

--- app/models/repo.js
+++ app/models/repo.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   created_at: DS.attr('date'),
   description: DS.attr('string'),
+  forks_count: DS.attr('number'),
   html_url: DS.attr('string'),
   name: DS.attr('string'),
   stargazers_count: DS.attr('number')
