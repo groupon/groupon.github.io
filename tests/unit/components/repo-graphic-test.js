466f39ca14f22302e23d51a0d6d3f3e8731c9d14
Jesse Pinho <jesse@jessepinho.com>
2016-02-25 00:00:09 -0600

    Add test for languageCells

--- tests/unit/components/repo-graphic-test.js
+++ tests/unit/components/repo-graphic-test.js
@@ -4,21 +4,22 @@ moduleForComponent('repo-graphic', 'Unit | Component | repo graphic', {
   needs: ['service:ajax']
 });
 
-test('bytesPerLanguageAsArray is the bytesPerLanguage object as an array', function(assert) {
-  let component = this.subject();
+test('languageCells has the correct number of cells per language', function(assert) {
+  let component = this.subject(),
+      javaScriptCells;
 
-  var expected = [
-    { language: "JavaScript", bytes: 50 },
-    { language: "CoffeeScript", bytes: 2 },
-  ];
+  component.set('bytesPerLanguage', { JavaScript: 45, CoffeeScript: 5 });
+  component.set('numberOfCells', 100);
 
-  component.set('bytesPerLanguage', { "JavaScript": 50, "CoffeeScript": 2 });
-  assert.deepEqual(component.get('bytesPerLanguageAsArray'), expected);
+  javaScriptCells = component.get('languageCells').filter(function(cell) {
+    return cell.language === 'JavaScript';
+  });
+  assert.equal(javaScriptCells.length, 90);
 });
 
 test('totalBytes is the sum of all byte counts', function(assert) {
   let component = this.subject();
-  component.set('bytesPerLanguage', { "JavaScript": 50, "CoffeeScript": 2 });
+  component.set('bytesPerLanguage', { JavaScript: 50, CoffeeScript: 2 });
 
   assert.equal(component.get('totalBytes'), 52);
 });
